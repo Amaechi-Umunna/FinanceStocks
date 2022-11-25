5edcd8871d098532a6af255fbd2f289296b5cd25
Amaechi-Umanna <100558243+Umunnabuike@users.noreply.github.com>
2022-11-25 11:38:53 +0100

    Initial Commit

---
+++ FinanceStocks/FinanceStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceStocksApp.swift
+//  FinanceStocks
+//
+//  Created by Amaechi Chukwu on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinanceStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
